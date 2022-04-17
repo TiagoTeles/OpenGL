042401925ba7523d3bf7ad8d475c5b2e02c9ce72
Tiago Moreira da Fonte Fonseca Teles <t.moreiradafontefonsecateles@student.tudelft.nl>
2022-04-17 15:11:55 +0200

    Added manufactured solution

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,11 +10,13 @@
 
 #define UNUSED(X) (void)(X)
 
-#define N 256
-#define ITER 10
+#define N 128
+#define ITER 16
 
 #define INDEX(X, Y) ((Y) * N + (X))
 #define SWAP(A, B) {float* C = A; A = B; B = C;}
+
+// TODO: REDUCE NUMBER OF VERTICES
 
 float orthographic[] = {
     0.5625f, 0.0f, 0.0f, 0.0f,
@@ -28,8 +31,9 @@ int main(int argc, char** argv) {
 
     // Physical Quantities
     float diff = 1E-6;      // Diffusivity, [m^2/s]
-    float size = 0.01;      // Length, [m]
     float velocity = 0.01;  // Velocity, [m/s]
+    float size = 0.01f;     // Length, [m]
+    float time = 0.00f;     // Elapsed Time, [s]
 
     // Discretizations
     float dx = size / N;
@@ -49,28 +53,18 @@ int main(int argc, char** argv) {
     float* vertices = new float[4*5 * N*N]();
     GLuint* indices = new GLuint[6 * N*N]();
 
-    for (int y = 0; y < N; y++) {
-        for (int x = 0; x < N; x++) {
-
-            // Setup velocity field
-            Vx0[INDEX(x, y)] = velocity;
-            Vy0[INDEX(x, y)] = velocity;
-
-            // Add dye
-            float radX = (2*((float) x/N) - 1.0f);
-            float radY = (2*((float) y/N) - 1.0f);
-
-            if (radX*radX + radY*radY < 0.01) {
-                rho0[INDEX(x, y)] = 100.0f;
-            }
-        }
-    }
-
     // Window
     Window window(1600, 900, "OpenGL");
 
     // Import shaders
     Shader shader("res/default.vert", "res/default.frag");
+
+    // Setup initial condition
+    for (int y = 0; y < N; y++) {
+        for (int x = 0; x < N; x++) {
+            rho0[INDEX(x, y)] = 100.0f * sin(0.1f*x) * sin(0.1f*y);
+        }
+    }
 
     // Run main loop
     double lastTime = glfwGetTime();
@@ -79,10 +73,37 @@ int main(int argc, char** argv) {
         // Get input events
         glfwPollEvents();
 
-        // Run simulation
-        float minDensity = NULL;
-        float maxDensity = NULL;
+        // Setup simulation
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < N; x++) {
+                Vx0[INDEX(x, y)] = velocity;
+                Vy0[INDEX(x, y)] = velocity;
 
+                float S = -sin(0.1f*x) * sin(0.1f*y) * sin(time) \
+                        + 0.1f * velocity * cos(0.1f*x) * sin(0.1f*y) * cos(time) \
+                        + 0.1f * velocity * sin(0.1f*x) * cos(0.1f*y) * cos(time) \
+                        + 0.01f * diff * sin(0.1f*x) * sin(0.1f*y) * cos(time) \
+                        + 0.01f * diff * sin(0.1f*x) * sin(0.1f*y) * cos(time);
+
+                rho[INDEX(x, y)] = rho0[INDEX(x, y)] + 100.0f * dt * S;
+            }
+        }
+
+        // Swap buffers
+        SWAP(rho, rho0);
+
+        // Apply B.C.
+        for (int x = 0; x < N; x++) {
+            rho[INDEX(x,   0)] = 100.0f * sin(0.1f*x) * sin(0.1f*0) * cos(time);
+            rho[INDEX(x, N-1)] = 100.0f * sin(0.1f*x) * sin(0.1f*N) * cos(time);
+        }
+
+        for (int y = 0; y < N; y++) {
+            rho[INDEX(  0, y)] = 100.0f * sin(0.1f*0) * sin(0.1f*y) * cos(time);
+            rho[INDEX(N-1, y)] = 100.0f * sin(0.1f*N) * sin(0.1f*y) * cos(time);
+        }
+
+        // Run simulation
         for (int i = 0; i < ITER; i++) {
             for (int y = 1; y < N-1; y++) {
                 for (int x = 1; x < N-1; x++) {
@@ -100,21 +121,32 @@ int main(int argc, char** argv) {
 
                     // Update density
                     rho[INDEX(x, y)] = (1/c) * (rho0[INDEX(x, y)] + dt * (convX + convY + diffX + diffY));
-
-                    // Check solution bounds
-                    if (rho[INDEX(x, y)] > maxDensity || maxDensity == NULL) {
-                        maxDensity = rho[INDEX(x, y)];
-                    }
-
-                    if (rho[INDEX(x, y)] < minDensity || minDensity == NULL) {
-                        minDensity = rho[INDEX(x, y)];
-                    }
                 }
             }
         }
 
-        printf("Max Density: %f\n", maxDensity);
-        printf("Min Density: %f\n", minDensity);
+        // Swap buffers
+        SWAP(rho, rho0);
+
+        // Calculate Error
+        float errorRMS = 0.0f;
+
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < N; x++) {
+                
+                // Manufactured and Exact solutions
+                float rhoMan = 100.0f * sin(0.1f*x) *  sin(0.1f*y) * cos(time);
+                float rhoDisc = rho0[INDEX(x, y)];
+                
+                // RMS Error
+                errorRMS += pow(rhoMan - rhoDisc, 2);
+            }
+        }
+
+        printf("RMS Error: %f\n", sqrt(errorRMS/pow(N, 2)));
+
+        // Update time
+        time += dt;
 
         // Draw density field
         for (int y = 0; y < N; y++) {
@@ -126,27 +158,27 @@ int main(int argc, char** argv) {
 
                 vertices[INDEX(x, y)*20 + 0*5 + 0] = x0;
                 vertices[INDEX(x, y)*20 + 0*5 + 1] = y0;
-                vertices[INDEX(x, y)*20 + 0*5 + 2] = rho[INDEX(x, y)] / 100.0f;
-                vertices[INDEX(x, y)*20 + 0*5 + 3] = rho[INDEX(x, y)] / 100.0f;
-                vertices[INDEX(x, y)*20 + 0*5 + 4] = rho[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 0*5 + 2] = rho0[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 0*5 + 3] = rho0[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 0*5 + 4] = rho0[INDEX(x, y)] / 100.0f;
 
                 vertices[INDEX(x, y)*20 + 1*5 + 0] = x1;
                 vertices[INDEX(x, y)*20 + 1*5 + 1] = y0;
-                vertices[INDEX(x, y)*20 + 1*5 + 2] = rho[INDEX(x, y)] / 100.0f;
-                vertices[INDEX(x, y)*20 + 1*5 + 3] = rho[INDEX(x, y)] / 100.0f;
-                vertices[INDEX(x, y)*20 + 1*5 + 4] = rho[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 1*5 + 2] = rho0[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 1*5 + 3] = rho0[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 1*5 + 4] = rho0[INDEX(x, y)] / 100.0f;
 
                 vertices[INDEX(x, y)*20 + 2*5 + 0] = x1;
                 vertices[INDEX(x, y)*20 + 2*5 + 1] = y1;
-                vertices[INDEX(x, y)*20 + 2*5 + 2] = rho[INDEX(x, y)] / 100.0f;
-                vertices[INDEX(x, y)*20 + 2*5 + 3] = rho[INDEX(x, y)] / 100.0f;
-                vertices[INDEX(x, y)*20 + 2*5 + 4] = rho[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 2*5 + 2] = rho0[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 2*5 + 3] = rho0[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 2*5 + 4] = rho0[INDEX(x, y)] / 100.0f;
 
                 vertices[INDEX(x, y)*20 + 3*5 + 0] = x0;
                 vertices[INDEX(x, y)*20 + 3*5 + 1] = y1;
-                vertices[INDEX(x, y)*20 + 3*5 + 2] = rho[INDEX(x, y)] / 100.0f;
-                vertices[INDEX(x, y)*20 + 3*5 + 3] = rho[INDEX(x, y)] / 100.0f;
-                vertices[INDEX(x, y)*20 + 3*5 + 4] = rho[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 3*5 + 2] = rho0[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 3*5 + 3] = rho0[INDEX(x, y)] / 100.0f;
+                vertices[INDEX(x, y)*20 + 3*5 + 4] = rho0[INDEX(x, y)] / 100.0f;
 
                 indices[INDEX(x, y)*6 + 0] = INDEX(x, y)*4 + 0;
                 indices[INDEX(x, y)*6 + 1] = INDEX(x, y)*4 + 1;
@@ -156,9 +188,6 @@ int main(int argc, char** argv) {
                 indices[INDEX(x, y)*6 + 5] = INDEX(x, y)*4 + 0;
             }
         }
-
-        // Swap buffers
-        SWAP(rho, rho0);
 
         // Create buffers
         VBO vbo(vertices, 4 * 5 * N*N * sizeof(float));
@@ -184,10 +213,23 @@ int main(int argc, char** argv) {
         glDrawElements(GL_TRIANGLES, 6*N*N, GL_UNSIGNED_INT, (const void*) 0);
         glfwSwapBuffers(window.window);
 
-        // Print FPS
-        printf("FPS: %.2f\n\n", 1.0f / (glfwGetTime() - lastTime));
-        lastTime = glfwGetTime();
+        // // Print FPS
+        // printf("FPS: %.2f\n\n", 1.0f / (glfwGetTime() - lastTime));
+        // lastTime = glfwGetTime();
     }
+
+    // Free Memory
+    delete Vx;
+    delete Vy;
+    delete rho;
+    delete Vx0;
+    delete Vy0;
+    delete rho0;
+    delete vertices;
+    delete indices;
+
+    shader.Delete();
+    window.Delete();
 
     return EXIT_SUCCESS;
 }
